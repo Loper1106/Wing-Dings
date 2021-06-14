202539ba9445ac9f366dc36c397d8fae5b082efa
Benjamin Loper <78445007+Loper1106@users.noreply.github.com>
2021-06-14 12:56:53 -0400

    Update main.py
    Moving screenshot function to 'runImage' file.

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
+import runImage
 from tkinter import *
 from PIL import Image, ImageTk
+import threading
 
 screen = Tk()
 screen.title("ChickieWings By: Ben Loper")
 screen.geometry("800x800")
 
 
-def screenshot():
-    print("Screenshot taken")
-
-
 def main():
     window = Label(screen, image="")
     window.pack()
 
-    screencap = Button(screen, text="Take Picture", command=screenshot)
+    screencap = Button(screen, text="Take Picture", command=lambda: runImage.screenshot())
     screencap.pack(side=BOTTOM)
+
+    # threading.Thread(target=runImage.cameraInit())
 
     screen.mainloop()
 
 
 if __name__ == '__main__':
     main()
-
-quit()
