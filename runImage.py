ed33fc24f1228052cea67b85eeec6c7b4ec06312
Benjamin Loper <78445007+Loper1106@users.noreply.github.com>
2021-06-14 00:25:46 -0400

    Update runImage.py
    Runs and displays video display

--- runImage.py
+++ runImage.py
@@ -1,7 +1,29 @@
+# https://docs.opencv.org/master/dd/d43/tutorial_py_video_display.html
 import cv2 as cv
 import sys
 
-img = cv.imread("doge.jpg")
-print(img)
+# Initializing Camera input
+camera = cv.VideoCapture('video.mp4')
 
+# Checks if Camera is visible
+if not camera.isOpened():
+    print("No camera detected...")
+
+# Read until video is completed
+while camera.isOpened():
+    # Capture frame-by-frame
+    ret, frame = camera.read()
+
+    if ret == True:
+        # Display the resulting frame
+        cv.imshow('Frame', frame)
+        # Press Q on keyboard to  exit
+        if cv.waitKey(24) == ord('q'):
+            break
+    # Break the loop
+    else:
+        break
+
+camera.release()
+cv.destroyAllWindows()
 quit()
