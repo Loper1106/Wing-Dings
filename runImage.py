b48ceec89e87c1cadb741852fcd813f6a1f1c8d3
Benjamin Loper <78445007+Loper1106@users.noreply.github.com>
2021-06-14 12:57:30 -0400

    Update runImage.py
    Adding camera into separate function; moved 'screenshot' function from main.

--- runImage.py
+++ runImage.py
@@ -1,30 +1,37 @@
 # https://docs.opencv.org/master/dd/d43/tutorial_py_video_display.html
 import cv2 as cv
 import sys
-import main
+import threading
 
-# Initializing Camera input
-camera = cv.VideoCapture(0)
 
-# Checks if Camera is visible
-if not camera.isOpened():
-    print("No camera detected...")
+def screenshot():
+    print("Screenshot taken")
 
-# Read until video is completed
-while camera.isOpened():
-    # Capture frame-by-frame
-    ret, frame = camera.read()
 
-    if ret == True:
-        # Display the resulting frame
-        cv.imshow('Frame', frame)
-        # Press Q on keyboard to  exit
-        if cv.waitKey(24) == ord('q'):
+def cameraInit():
+    # Initializing Camera input
+    camera = cv.VideoCapture(0)
+    # Checks if Camera is visible
+    if not camera.isOpened():
+        print("No camera detected...")
+
+    # Read until video is completed
+    while camera.isOpened():
+        # Capture frame-by-frame
+        ret, frame = camera.read()
+
+        if ret:
+            # Display the resulting frame
+            cv.imshow('Frame', frame)
+            # Press Q on keyboard to  exit
+            if cv.waitKey(24) == ord('q'):
+                break
+        # Break the loop
+        else:
             break
-    # Break the loop
-    else:
-        break
 
-camera.release()
-cv.destroyAllWindows()
-quit()
+    camera.release()
+    cv.destroyAllWindows()
+
+
+# cameraInit()
