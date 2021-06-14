e97da50cfa5452b78d0c39788925dbcff4fa1013
Benjamin Loper <78445007+Loper1106@users.noreply.github.com>
2021-06-14 13:29:36 -0400

    Update main.py
    Displays image read from runimage.py

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ screen.geometry("800x800")
 
 
 def main():
-    window = Label(screen, image="")
+    picture = ImageTk.PhotoImage(runImage.image_from_frame)
+    window = Label(screen, image=picture)
     window.pack()
 
     screencap = Button(screen, text="Take Picture", command=lambda: runImage.screenshot())
     screencap.pack(side=BOTTOM)
 
-    threading.Thread(target=runImage.cameraInit())
-
     screen.mainloop()
 
 
 if __name__ == '__main__':
-    main()
+    threading.Thread(target=runImage.cameraInit())
+    threading.Thread(target=main())
