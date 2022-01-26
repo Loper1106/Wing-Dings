4ee24a6d9835fbebd8d9064e588ade01dea2b667
Benjamin Loper <78445007+Loper1106@users.noreply.github.com>
2022-01-26 16:22:18 -0500

    Update customVision.py
    Authenticating Client

--- venv/customVision.py
+++ venv/customVision.py
@@ -8,3 +8,9 @@ ENDPOINT = "https://loperfinalproject.cognitiveservices.azure.com/"
 training_key = "93a938acb00d4ef7bd67f10e51edec5d"
 prediction_key = "1e4442af34a249f7b4b52844ef9cc9ff"
 prediction_resource_id = "/subscriptions/222f0e67-4426-4362-aa44-63916a12d2e5/resourceGroups/AzDevCloudBadgeRG-BL/providers/Microsoft.CognitiveServices/accounts/LoperFinalProject"
+
+# Authenticate Client
+credentials = ApiKeyCredentials(in_headers={"Training-key": training_key})
+trainer = CustomVisionTrainingClient(ENDPOINT, credentials)
+prediction_credentials = ApiKeyCredentials(in_headers={"Prediction-key": prediction_key})
+predictor = CustomVisionPredictionClient(ENDPOINT, prediction_credentials)
