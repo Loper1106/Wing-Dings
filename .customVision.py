c6c2647f5d15c527f7b1ed807ebeeb35d14bc2e0
Benjamin Loper <78445007+Loper1106@users.noreply.github.com>
2022-01-27 20:43:25 -0500

    Temp Ignore
    May no longer be needed

--- .customVision.py
+++ .customVision.py
@@ -6,9 +6,13 @@ from msrest.authentication import ApiKeyCredentials
 import os, time, uuid
 
 ENDPOINT = "https://loperfinalproject.cognitiveservices.azure.com/"
-training_key = "93a938acb00d4ef7bd67f10e51edec5d"
+training_key = "1e4442af34a249f7b4b52844ef9cc9ff"
 prediction_key = "1e4442af34a249f7b4b52844ef9cc9ff"
-prediction_resource_id = "/subscriptions/222f0e67-4426-4362-aa44-63916a12d2e5/resourceGroups/AzDevCloudBadgeRG-BL/providers/Microsoft.CognitiveServices/accounts/LoperFinalProject"
+prediction_resource_id = "/subscriptions/222f0e67-4426-4362-aa44-63916a12d2e5/resourceGroups/AzDevCloudBadgeRG-BL/" \
+                         "providers/Microsoft.CognitiveServices/accounts/LoperFinalProject"
+
+# Initialize Customer Vision Project
+publish_iteration_name = "classifying Model"
 
 # Authenticate Client
 credentials = ApiKeyCredentials(in_headers={"Training-key": training_key})
@@ -16,10 +20,12 @@ trainer = CustomVisionTrainingClient(ENDPOINT, credentials)
 prediction_credentials = ApiKeyCredentials(in_headers={"Prediction-key": prediction_key})
 predictor = CustomVisionPredictionClient(ENDPOINT, prediction_credentials)
 
-# Initialize Customer Vision Project
-publish_iteration_name = "classifying Model"
-
 # Create New Project
 print("Creating project")
 project_name = uuid.uuid4()
+str(project_name)
+print(project_name)
 project = trainer.create_project(project_name)
+
+# Create Project  Tags
+
